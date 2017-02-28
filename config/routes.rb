ea81b41f41f5f0c79f39f03c315ff0f4c5186ed4
linto <johnjohnythayyil@gmail.com>
2017-03-01 00:59:47 +0530

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
 	
    root 'static_pages#home'
 
-  	get 'static_pages/home'
 
-    get 'static_pages/about'
+    get '/about', to: 'static_pages#about'
 
-    get 'static_pages/company'
+    get '/company', to: 'static_pages#company'
 
-  	get  'static_pages/help'
+  	get  '/help', to: 'static_pages#help'
 
-  	get 'static_pages/contact'
+  	get '/contact' to: 'static_pages#contact'
   	
-	root 'static_pages#home'
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -15,35 +15,35 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", " home | #{@base_title}"
 
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", " about | #{@base_title}"
 
   end
 
   test "should get company" do
-    get static_pages_company_url
+    get company_path
     assert_response :success
     assert_select "title", " company | #{@base_title}"
 
   end
 
     test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", " help | #{@base_title}"
 
   end
 
     test "should get contact" do
-    get static_pages_help_url
+    get contact_path
     assert_response :success
     assert_select "title", "contact | #{@base_title}"
 
